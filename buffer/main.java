10c37135f23384b4d0aab35ff285df7561a0be9c
Fiakx <darkfast75@gmail.com>
2026-09-20 22:54:16 +0200

    I had forgot the 'this.' in the class

--- buffer/main.java
+++ buffer/main.java
@@ -4,43 +4,47 @@
 
 
 class arr {
+    /*
+    
+    In this class accf represent the next "case", not the actuall end case
+     */
     int[] arr;
     int accd = 0;
     int accf = -1;
     public void print(){
-        for ( int i = 0 ; i<arr.length; i++){
-            System.out.print("|"+ arr[i]);
+        for ( int i = 0 ; i<this.arr.length; i++){
+            System.out.print("|"+ this.arr[i]);
         }
         System.out.print("|\n");
-        for ( int i = 0; i < (accd)%(arr.length); i ++){
+        for ( int i = 0; i < (this.accd)%(arr.length); i ++){
             System.out.print("  ");
         }
         System.out.print(" ^\n");
-        for ( int i = 0; i < (accd)%(arr.length); i ++){
+        for ( int i = 0; i < (this.accd)%(arr.length); i ++){
             System.out.print("  ");
         }
         System.out.print(" | start\n");
-        for ( int i = 0; i < (accf-1)%(arr.length-1); i ++){
+        for ( int i = 0; i < (this.accf-1)%(arr.length-1); i ++){
             System.out.print("  ");
         }
         System.out.print(" ^\n");
-        for ( int i = 0; i < (accf-1)%(arr.length-1); i ++){
+        for ( int i = 0; i < (this.accf-1)%(arr.length-1); i ++){
             System.out.print("  ");
         }
         System.out.print(" | end\n");
 
     }
     public void add(int elem){
-        if (accf==-1){
-            arr[0]=elem;
-            accf=1;
-        }else if ((accf)%(arr.length)!=accd){
-            if (arr.length-1 != accf-1){
-                arr[accf]=elem;
-                accf+=1;
+        if (this.accf==-1){
+            this.arr[0]=elem;
+            this.accf=1;
+        }else if ((this.accf)%(this.arr.length)!=this.accd){
+            if (this.arr.length-1 != this.accf-1){
+                this.arr[this.accf]=elem;
+                this.accf+=1;
             }else{
-                arr[0]=elem;
-                accf=0;
+                this.arr[0]=elem;
+                this.accf=0;
             }
         }else{
             System.out.println("It's not possible to add a number : arr is full");
@@ -50,10 +54,10 @@ class arr {
     }
 
     public void del(){
-        if (accd!=0){
-            accd-=1;
+        if (this.accd!=0){
+            this.accd-=1;
         }else{
-            accd = arr.length -1;
+            this.accd = this.arr.length -1;
         }
     }
 
